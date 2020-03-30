6f88a37396068200875ac159401bb3042f1d6901
laidi-abdelhalim <abdelhalim@onidivo.com>
2020-03-31 00:16:04 +0100

    covid-kr, covid-pt: update output format

--- covid-pt/src/main.js
+++ covid-pt/src/main.js
@@ -8,14 +8,14 @@ Apify.main(async () => {
     const kvStore = await Apify.openKeyValueStore('COVID-19-PT');
     const dataset = await Apify.openDataset('COVID-19-PY-HISTORY');
 
-    const browser = await Apify.launchPuppeteer({useApifyProxy: true, apifyProxyGroups: ['SHADER']});
+    const browser = await Apify.launchPuppeteer({ useApifyProxy: true, apifyProxyGroups: ['SHADER'] });
     const page = await browser.newPage();
     await Apify.utils.puppeteer.injectJQuery(page);
     await Apify.utils.puppeteer.blockRequests(page, {
         urlPatterns: [".jpg", ".jpeg", ".png", ".svg", ".gif", ".woff", ".pdf", ".zip"]
     });
 
-    await page.goto(url, {waitUntil: 'networkidle0', timeout: 1000 * 600});
+    await page.goto(url, { waitUntil: 'networkidle0', timeout: 1000 * 600 });
     await page.waitForSelector('full-container')
 
     const extracted = await page.evaluate(() => {
@@ -25,7 +25,7 @@ Apify.main(async () => {
                 const value = $(p).find('strong').first().text().trim();
                 const key = $(p).find('strong').last().text().trim();
                 infectedByRegion.push({
-                    value,
+                    value: parseInt(value),
                     region: key
                 })
             }
@@ -35,10 +35,10 @@ Apify.main(async () => {
         const full_container = $('full-container').first().children();
 
         const date = full_container.find("div:contains(Dados relativos ao boletim da DGS de)").eq(2).find('g').last().text().trim()
-        const totalTested = full_container.eq(4).find('g').last().text().replace(/(\n|,| )/g, '');
+        const tested = full_container.eq(4).find('g').last().text().replace(/(\n|,| )/g, '');
         const infected = full_container.eq(1).find('g').last().text().replace(/(\n|,| )/g, '');
         const recovered = full_container.eq(2).find('g').last().text().replace(/(\n|,| )/g, '');
-        const deaths = full_container.eq(3).find('g').last().text().replace(/(\n|,| )/g, '');
+        const deceased = full_container.eq(3).find('g').last().text().replace(/(\n|,| )/g, '');
 
         const $ps = full_container.eq(11).find('nav p').toArray();
 
@@ -46,25 +46,34 @@ Apify.main(async () => {
         const infectedByRegion = getInfectedByRegion($ps);
 
         return {
-            date, totalTested, infected, recovered, deaths, infectedByRegion
+            date, tested, infected, recovered, deceased, infectedByRegion
         };
     });
 
-    let {date, totalTested, infected, recovered, deaths, infectedByRegion} = extracted;
+    let { date, tested, infected, recovered, deceased, infectedByRegion } = extracted;
     let sourceDate = new Date(formatDate(date));
 
-    // ADD: totalTested, infected, recovered, deaths
-    const data = {totalTested, infected, recovered, deaths}
 
-    let latest = await kvStore.getValue(LATEST);
+    // ADD: totalTested, infected, recovered, deaths
+    const data = {
+        infected: infected || infected === '0' ? parseInt(infected) : 'N/A',
+        tested: tested || tested === '0' ? parseInt(tested) : 'N/A',
+        recovered: recovered || recovered === '0' ? parseInt(recovered) : 'N/A',
+        deceased: deceased || deceased === '0' ? parseInt(deceased) : null
+    }
+
 
     // ADD: infectedByRegion, lastUpdatedAtApify, lastUpdatedAtSource
     if (infectedByRegion && infectedByRegion.length) data.infectedByRegion = infectedByRegion;
+    data.country = 'Portugal';
+    data.moreData = 'https://api.apify.com/v2/key-value-stores/NDqcdJ53aN730Kb1Q/records/LATEST?disableRedirect=true';
+    data.historyData = 'https://api.apify.com/v2/datasets/VeXjF7u71PU8IO6NH/items?format=json&clean=1';
     data.sourceUrl = 'https://covid19.min-saude.pt/ponto-de-situacao-atual-em-portugal/';
     data.lastUpdatedAtApify = new Date(Date.UTC(now.getFullYear(), now.getMonth(), now.getDate(), now.getHours(), now.getMinutes())).toISOString();
     if (sourceDate != 'Invalid Date') data.lastUpdatedAtSource = new Date(Date.UTC(sourceDate.getFullYear(), sourceDate.getMonth(), sourceDate.getDate(), sourceDate.getHours(), sourceDate.getMinutes())).toISOString();
     data.readMe = 'https://apify.com/onidivo/covid-pt';
 
+    let latest = await kvStore.getValue(LATEST);
     if (!latest) {
         await kvStore.setValue('LATEST', data);
         latest = data;
